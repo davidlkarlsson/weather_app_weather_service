1dfa2b96b4074acf8653aa74e2367f32cd83e7ce
David Karlsson <davidfkarlsson@proton.me>
2025-11-19 11:24:29 +0100

    initial commit

---
+++ src/main/java/com/david/weather_app_weather_service/WeatherAppWeatherServiceApplication.java
@@ -0,0 +1,13 @@
+package com.david.weather_app_weather_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAppWeatherServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAppWeatherServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/david/weather_app_weather_service/WeatherAppWeatherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.weather_app_weather_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAppWeatherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
